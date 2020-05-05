74ec4947472273fae80bfffb0aec17f53d6e96ad
Charles Black <18634426+charlesoblack@users.noreply.github.com>
2020-05-05 17:01:09 -0500

    add a few cli options

--- train.py
+++ train.py
@@ -1,22 +1,36 @@
 #! /usr/bin/env python3
 
 import os
-from src import initialize_model_for_training_file, train_yolo
+from src import initialize_model_for_training_file as model_init, train_yolo
 import torch
 import torchvision
 from helper import collate_fn
 from data_helper import LabeledDataset
 import numpy as np
+import argparse
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--n_epochs', type=int, default=10)
+parser.add_argument('--batch_size', type=int, default=1)
+parser.add_argument('--lr', type=float, default=1e-4)
+parser.add_argument('--pretrain', action='store_true')
+parser.add_argument('--verbose', action='store_true')
+opt = parser.parse_args()
 
 cuda = torch.cuda.is_available()
 device = 'cuda:0' if cuda else 'cpu'
 
-# kobe_model = KobeModel(num_classes=10, encoder_features=6, rm_dim=800)
-kobe_model = initialize_model_for_training_file('pretrain_model_2_epochs.pt')
+if opt.pretrain:
+    kobe_model = model_init('pretrain_model_2_epochs.pt')
+else:
+    from src import KobeModel
+
+    kobe_model = KobeModel(num_classes=10, encoder_features=6, rm_dim=800)
+
 kobe_model.to(device)
 
-lr = 0.0003
+lr = opt.lr
 b1 = 0.9
 b2 = 0.999
 
@@ -24,7 +38,7 @@ kobe_optimizer = torch.optim.Adam(kobe_model.parameters(),
                                   lr=lr,
                                   betas=(b1, b2))
 
-n_epochs = 10
+n_epochs = opt.n_epochs
 
 image_folder = 'data'
 annotation_csv = 'data/annotation.csv'
@@ -39,7 +53,7 @@ labeled_trainset = LabeledDataset(image_folder=image_folder,
                                   extra_info=False,
                                   )
 trainloader = torch.utils.data.DataLoader(labeled_trainset,
-                                          batch_size=1,
+                                          batch_size=opt.batch_size,
                                           shuffle=True,
                                           num_workers=0,
                                           collate_fn=collate_fn,
@@ -48,12 +62,12 @@ trainloader = torch.utils.data.DataLoader(labeled_trainset,
 
 for epoch in range(n_epochs):
     print("EPOCH: {}".format(epoch))
-    train_yolo(trainloader, kobe_model, kobe_optimizer)
+    train_yolo(trainloader, kobe_model, kobe_optimizer, opt.verbose)
 
     torch.save(kobe_model.state_dict(),
                f'ke_model_w_pretrain2_high_lr_{epoch}_epochs.pt')
     try:
+        # keep the last 3 epochs and remove any previous ones
         os.remove(f'ke_model_w_pretrain2_high_lr_{epoch - 3}_epochs.pt')
-        os.remove(f'ke_model_w_pretrain2_high_lr_{epoch - 4}_epochs.pt')
     except FileNotFoundError:
         pass
