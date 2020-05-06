01beadcc68a882bf78afe881a87d6cc8d6fc696d
Charles Black <18634426+charlesoblack@users.noreply.github.com>
2020-05-05 19:06:18 -0500

    return iou_max in evaluate

--- run_test.py
+++ run_test.py
@@ -77,7 +77,7 @@ with torch.no_grad():
         sample = sample.cuda()
 
         predicted_bounding_boxes = model_loader.get_bounding_boxes(sample)[0].cpu()
-        ats_bounding_boxes = compute_ats_bounding_boxes(predicted_bounding_boxes, target['bounding_box'][0])
+        ats_bounding_boxes, iou_max = compute_ats_bounding_boxes(predicted_bounding_boxes, target['bounding_box'][0])
         total_ats_bounding_boxes += ats_bounding_boxes
 
         if opt.verbose:
