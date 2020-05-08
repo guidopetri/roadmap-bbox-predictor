ebe4ee2b600afc16e35f01cd7a2761ce4f386c52
Esteban Navarro Garaiz <eng272@nyu.edu>
2020-05-07 23:13:58 -0400

    Return so that it runs evaluation on all images

--- evaluate.py
+++ evaluate.py
@@ -63,8 +63,7 @@ print(f'Args: {opt}')
 image_folder = opt.data_dir
 annotation_csv = f'{opt.data_dir}/annotation.csv'
 
-#labeled_scene_index = np.arange(120, 134)
-labeled_scene_index = np.arange(120, 121)
+labeled_scene_index = np.arange(120, 134)
 
 # For bounding boxes task
 labeled_trainset_task1 = LabeledDataset(
