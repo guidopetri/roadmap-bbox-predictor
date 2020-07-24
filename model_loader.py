069327497c09775362ccacc98b53101f4be7a1e0
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2020-07-24 18:53:20 -0500

    combined model as default

--- model_loader.py
+++ model_loader.py
@@ -35,7 +35,7 @@ class ModelLoader():
     team_member = ['Nabeel Sarwar', 'Esteban Navarro Garaiz', 'Guido Petri']
     contact_email = 'gp1655@nyu.edu'
 
-    def __init__(self, model_file='kobe_model_w_pretrain2_9_epochs.pt', prob_thresh=0.1, conf_thresh=0.1, nms_thresh=0.4, batch_norm=False, shared_decoder=False):
+    def __init__(self, model_file='combined_model.pt', prob_thresh=0.1, conf_thresh=0.1, nms_thresh=0.4, batch_norm=False, shared_decoder=False):
 
         # You should
         #       1. create the model object
